fcf9eb7f8f465de04a799f6569ba0325762b20a4
pedroPolonea <phpolonea@hotmail.com>
2021-01-31 22:18:06 -0300

    Introduçao stateless

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/cupertino.dart';
 
 main() {
-  runApp(
-    Container(
-      child: Text(
-        'Polo', 
-        textDirection: TextDirection.ltr,
-        style: TextStyle( 
-          
-          fontSize: 50.0
-          ),
-      ),
-  ));
+  runApp(AppWidget(title: 'Polo',));
+}
+
+class AppWidget extends StatelessWidget{
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return   Container(
+                child: Center (
+                  child: Text(
+                    title, 
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle( 
+                      
+                      fontSize: 50.0
+                      ),
+                  ),
+                )
+            );
+  }
+  
 }
