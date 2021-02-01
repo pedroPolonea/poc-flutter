b9468211c98d5cb9ff3c1ff593bf8b890124a947
pedroPolonea <phpolonea@hotmail.com>
2021-01-31 22:22:35 -0300

    Introduçao MaterialApp

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 main() {
   runApp(AppWidget(title: 'Polo',));
@@ -12,18 +13,16 @@ class AppWidget extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return   Container(
-                child: Center (
-                  child: Text(
-                    title, 
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle( 
-                      
-                      fontSize: 50.0
-                      ),
-                  ),
-                )
-            );
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.red
+      ),
+      home: Container(
+        child: Center(
+          child: Text('Polo'),
+        ),
+      ),
+    );
   }
   
 }
