8394717cd00ef642e5bb03e3df93a12caa83012b
pedroPolonea <phpolonea@hotmail.com>
2021-01-31 22:39:12 -0300

    Introduçao SOC

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,54 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 main() {
   runApp(AppWidget(title: 'Polo',));
 }
-
-class AppWidget extends StatelessWidget{
-  final String title;
-
-  const AppWidget({Key key, this.title}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.red
-      ),
-      home: HomePage(),
-    );
-  }
-
-}
-
-class HomePage extends StatefulWidget{
-
-  @override
-  State<StatefulWidget> createState() {
-    return HomePageState();    
-  }
-
-}
-
-class HomePageState extends State<HomePage>{
-  int cont = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-          child: GestureDetector(
-            child:  Text('Polo $cont'),
-            onTap: (){
-              setState(() {
-                cont++;
-              });
-            },
-            )
-        )
-      );
-  }
-
-}
-
