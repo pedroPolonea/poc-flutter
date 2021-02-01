0c63f756e138c0ac19ef86e4e369a40dabf55d4e
pedroPolonea <phpolonea@hotmail.com>
2021-01-31 23:29:42 -0300

    Introduçao render 2

--- ola_mundo/lib/app_widget.dart
+++ ola_mundo/lib/app_widget.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.red
-      ),
-      home: HomePage(),
+    return AnimatedBuilder(
+      // ignore: missing_return
+      animation: AppController.instance,
+      builder: (context, chid){
+         return MaterialApp(
+            home: HomePage(),
+            theme: ThemeData(
+              primaryColor: Colors.red,
+              brightness: AppController.instance.isDarkTheme ? Brightness.dark : Brightness.light,
+            ),
+        );
+      }
     );
   }
 
--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 class HomePage extends StatefulWidget{
 
@@ -12,22 +13,20 @@ class HomePage extends StatefulWidget{
 
 class HomePageState extends State<HomePage>{
   int cont = 0;
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('App'),
       ),
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.black,
-        child: Center(
-            child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.cyan,
-          )
+      body: Center(
+        child: Switch(
+          value: AppController.instance.isDarkTheme,
+          onChanged: (value){
+            AppController.instance.changeTheme();
+            print(value);
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
