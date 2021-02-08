cb0e770cbdce862d9da755ae1de4a8c88abdb792
pedroPolonea <phpolonea@hotmail.com>
2021-02-08 00:20:03 -0300

    Ajustes layout

--- conversor_curso/lib/app/views/home_view.dart
+++ conversor_curso/lib/app/views/home_view.dart
@@ -7,30 +7,76 @@ class HomeView extends StatelessWidget {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            Image.asset('assets/cm.jpg'),
-            Row(
-              children: [
-                Expanded(
-                  child: DropdownButton(
-                    items: [], 
-                    onChanged: (value){},
-                  )
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 100, 
+            left: 20, 
+            right: 20, 
+            bottom: 20,
+          ),
+          child: Column(
+            children: [ 
+              Image.asset(
+                'assets/cm.jpg', 
+                width: 200, 
+                height: 200,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: SizedBox(
+                        height: 56,
+                        child: DropdownButton(
+                          isExpanded: true,
+                          underline: Container(
+                            height: 1,
+                            color: Colors.amber,
+                          ),
+                          items: [
+                            DropdownMenuItem(
+                              child: Text('Real')
+                            ),
+                            DropdownMenuItem(
+                              child: Text('Dolar')
+                            ),
+                          ], 
+                          onChanged: (value){},
+                        ),
+                      )
+                    ),
+                    SizedBox(width: 10,),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.amber
+                            )
+                          ),
+                          focusedBorder:  UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.amber
+                            )
+                          ),
+                        ),
+                      )
+                    )
+                  ],
                 ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(),
-                  )
-                )
-              ],
-            ),
-            RaisedButton(
-              onPressed: (){},
-              child: Text('CONVERTER'),           
-            )
-            
-          ],
+              ),
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: (){},
+                child: Text('CONVERTER'),           
+              )
+              
+            ],
+          ),
         ),
       )
     );
